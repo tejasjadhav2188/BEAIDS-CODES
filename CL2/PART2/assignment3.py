8cf2a9a250329b2a4f1530121b4e5af7b3be261a
Moin Sayyed <sayyedmoin4902@gmail.com>
2023-08-30 14:20:10 +0530

    updated Assignment3 with required CSV

--- CL2/PART2/assignment3.py
+++ CL2/PART2/assignment3.py
@@ -1,7 +1,6 @@
 import numpy as np
-import csv
 import pandas as pd
-from pgmpy.models import BayesianModel
+from pgmpy.models import BayesianNetwork
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.inference import VariableElimination
 
@@ -14,8 +13,8 @@ print('Few examples from the dataset are given below')
 print(heartDisease.head())  # Display the first few rows of the dataset
 
 # Model Bayesian Network
-model = BayesianModel([
-    ('age', 'trestbps'), ('age', 'fbs'), ('sex', 'trestbps'), ('exang', 'trestbps'),
+model = BayesianNetwork([
+    ('age', 'trestbps'), ('age', 'fbs'), ('gender', 'trestbps'), ('exang', 'trestbps'),
     ('trestbps', 'heartdisease'), ('fbs', 'heartdisease'), ('heartdisease', 'restecg'),
     ('heartdisease', 'thalach'), ('heartdisease', 'chol')
 ])  # Define the structure of the Bayesian Network
@@ -29,11 +28,11 @@ print('\nInferencing with Bayesian Network:')
 HeartDisease_infer = VariableElimination(model)  # Create an inference engine using VariableElimination
 
 # Computing the Probability of HeartDisease given Age
-print('\n1. Probability of HeartDisease given Age=28')
-q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 28})  # Perform probabilistic inference
-print(q['heartdisease'])  # Print the probability distribution
+print('\n1. Probability of HeartDisease given Age=40')  # Changed age to 40
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'age': 40})  # Perform probabilistic inference
+print(q.values)  # Print the probability distribution
 
 # Computing the Probability of HeartDisease given cholesterol
 print('\n2. Probability of HeartDisease given cholesterol=100')
-q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'chol': 100})  # Perform probabilistic inference
-print(q['heartdisease'])  # Print the probability distribution
+q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'chol': 180})  # Perform probabilistic inference
+print(q.values)  # Print the probability distribution
